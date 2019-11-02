5bc389fc10e7c07407ec4efe480404d468dae788
B034333-2019 <s1239445@ed.ac.uk>
2019-11-02 08:00:47 +0000

    Upload before starting work today

--- runme.py
+++ runme.py
@@ -15,7 +15,7 @@ tg_var = ""
 
 #test_pfn: function that checks the user input for the protein family name is a string, and not a number or logic operator
 
-def test_pfn(pfn_var):
+def test_pfn():
     pfn_var = input("Please enter the protein family name you are interested in. Capitalisation is not important, but spelling is: ")
     while True:
         pfn_var = pfn_var.lower()
@@ -34,7 +34,9 @@ def test_pfn(pfn_var):
                 print("Please input Y ('Yes') or N ('No').")
 
 
-def test_tg(tg_var):
+#test_pfn: function that checks the user input for the taxonomy is a string, and not a number or logic operator
+
+def test_tg():
     tg_var = input("Please enter the taxonomic group you are interested in. Again, capitalisation is not important, but spelling is: ")
     while True:
         tg_var = tg_var.lower()
@@ -47,26 +49,50 @@ def test_tg(tg_var):
                 return tg_var
                 break
             elif y_n_query.upper() == "N":
-                tg_var = input("Please enter the protein family name you are interested in: ")
+                tg_var = input("Please enter the taxonomic group you are interested in: ")
                 continue
             else:
                 print("Please input Y ('Yes') or N ('No').")
                
 
 #Execute both test_pfn and test_tg functions to retrieve user input of protein families and taxonomic group
-           
-pfn_var = test_pfn(pfn_var)
-tg_var = test_tg(tg_var)
+#%%
 
+def search_database(pfn_var, tg_var):
+    touple = (pfn_var, tg_var)
+    correct_search = input("Searching for " + pfn_var.capitalize() + " in the taxonomy " + tg_var.capitalize() + " in the NCBI database. Is this correct? Y/N")
+    while True:
+        if correct_search.upper() == "Y":
+            return touple
+        if correct_search.upper() == "N":
+            print("Please run script again to input your protein family name and taxonomic group of interest.")
+            break
+        else:
+            print("Please input Y ('Yes') or N ('No').")
+
+
+#%%
+pfn_var = test_pfn()
+tg_var = test_tg()
+final_search = search_database(pfn_var, tg_var)
+
+print(final_search)
+
+#%%
 #%% - end 
 
 #Print to console what search choices are about to be made. Maybe include italics and capitals?
 
-print("Searching for " + pfn_var.capitalize() + " in the taxonomy " + tg_var.capitalize() + " in the NCBI database.")
-touple = (pfn_var, tg_var)
 
-print(type(touple))
 
-print(touple)
+#print("Searching for " + pfn_var.capitalize() + " in the taxonomy " + tg_var.capitalize() + " in the NCBI database.")
+#touple = (pfn_var, tg_var)
 
-print(touple[0], touple[1])
+#print(type(touple))
+
+#print(touple)
+
+#print(touple[0], touple[1])
+
+#%%
+
