8c026d3366463cc3b2342c34fd04e4411d2a6e8d
B034333-2019 <s1239445@ed.ac.uk>
2019-11-17 21:10:56 +0000

    Final commit before creating passworded ZIP file for upload.

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Cleans up folder leaving only desired output files behind
+
+echo -e "Running cleanup..."
+
+rm -f count 
+rm -f variables.txt
+rm -f temp.fasta
+rm -f prositereport
+rm -f twofifty_final_fastas
+rm -f 250_headers_only
+rm -f 250_seq.txt
+rm -f blastp_output
+rm -f seq_db.psq
+rm -f seq_db.phr
+rm -f seq_db.pin
